f37c2bbc57842fbe4435cbba24fe30d39b2f28f0
GregoryF27 <gregory.friedman@uconn.edu>
2024-08-26 23:46:36 -0400

    Implementation of decrypt_message_two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,13 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+
+decrypted_message = list(encrypted_message)
+for i in range(1,len(decrypted_message)//2,2):
+    j = len(decrypted_message)-i-1
+    decrypted_message[i], decrypted_message[j] = decrypted_message[j], decrypted_message[i]
+    
+    
+decrypted_message = ''.join(decrypted_message)
+print(decrypted_message)
